ab862a6b425de8774c9136e4e3eda3ea79a69a1e
ZeroTech <shelikov98@yandex.ru>
2021-04-06 13:11:17 +0800

    Create database connection, remove MainWindow

---
+++ JuniorSlataTestTask/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuniorSlataTestTask
+{
+    internal class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Slata");
+        }
+    }
+}
---
+++ JuniorSlataTestTask/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuniorSlataTestTask
+{
+    internal class User
+    {
+        public int id { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+        public string permission { get; set; }
+    }
+}
