fa117525552de6950c0c45c61d2376aec22a90a0
ZeroTech <shelikov98@yandex.ru>
2021-04-07 12:45:33 +0800

    Create user registration process to SQL Server

--- JuniorSlataTestTask/Data/Models/User.cs
+++ JuniorSlataTestTask/Data/Models/User.cs
@@ -7,10 +7,55 @@ namespace JuniorSlataTestTask
     internal class User
     {
         public int id { get; set; }
-        public string login { get; set; }
-        public string password { get; set; }
-        public string name { get; set; }
-        public string surname { get; set; }
-        public string permission { get; set; }
+        private string login, password, name, surname, permission;
+
+        public string Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public string Permission
+        {
+            get { return permission; }
+            set { permission = value; }
+        }
+
+        public User()
+        {
+        }
+
+        public User(string login, string password, string name, string surname)
+        {
+            Login = login;
+            Password = password;
+            Name = name;
+            Surname = surname;
+            Permission = "User";
+        }
+
+        public void ChangePermissions(string newPerm)
+        {
+            if (String.IsNullOrEmpty(newPerm)) return;
+            Permission = newPerm;
+        }
     }
 }
--- JuniorSlataTestTask/RegWindow.xaml.cs
+++ JuniorSlataTestTask/RegWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,8 @@ namespace JuniorSlataTestTask
     {
         public event EventHandler WindowClosed;
 
+        private AppContext dbcontext;
+
         protected virtual void OnWindowClosed(EventArgs e)
         {
             EventHandler handler = WindowClosed;
@@ -31,10 +35,63 @@ namespace JuniorSlataTestTask
         public RegWindow()
         {
             InitializeComponent();
+            dbcontext = new AppContext();
         }
 
         private void Register_ButtonClick(object sender, RoutedEventArgs e)
         {
+            string login = tbLogin.Text.Trim();
+            string password = tbPass.Password == tbPassCheck.Password ? tbPass.Password.Trim() : "";
+            string name = tbName.Text.Trim();
+            string surname = tbSurname.Text.Trim();
+
+            if (login.Length < 5 || String.IsNullOrEmpty(login) ||
+                new Regex("[^a-zA-Z0-9]").IsMatch(login) || new Regex("[^a-zA-Z]").IsMatch(login.First().ToString()))
+            {
+                tbLogin.ToolTip = "Неверный ввод даных!";
+                tbLogin.Background = Brushes.LightPink;
+            }
+            else if (password.Length < 8 || String.IsNullOrEmpty(password) ||
+                new Regex("[^a-zA-Z0-9]").IsMatch(password))
+            {
+                tbPass.ToolTip = "Неверный ввод даных";
+                tbPass.Background = Brushes.LightPink;
+                tbPassCheck.ToolTip = "Неверный ввод даных";
+                tbPassCheck.Background = Brushes.LightPink;
+            }
+            else if (String.IsNullOrEmpty(name) ||
+                new Regex("[^a-zA-Zа-яА-Я]").IsMatch(name))
+            {
+                tbName.ToolTip = "Неверный ввод даных";
+                tbName.Background = Brushes.LightPink;
+            }
+            else if (String.IsNullOrEmpty(surname) ||
+                new Regex("[^a-zA-Zа-яА-Я]").IsMatch(surname))
+            {
+                tbSurname.ToolTip = "Неверный ввод даных";
+                tbSurname.Background = Brushes.LightPink;
+            }
+            else
+            {
+                foreach (UIElement el in continerReg.Children)
+                {
+                    if (el is TextBox)
+                    {
+                        ((TextBox)el).ToolTip = "";
+                        ((TextBox)el).Background = Brushes.Transparent;
+                    }
+                    if (el is PasswordBox)
+                    {
+                        ((PasswordBox)el).ToolTip = "";
+                        ((PasswordBox)el).Background = Brushes.Transparent;
+                    }
+                }
+                dbcontext.Users.Add(new User(login, password, name, surname));
+                dbcontext.SaveChanges();
+
+                Application.Current.MainWindow.Show();
+                this.Close();
+            }
         }
 
         private void SignIn_ButtonClick(object sender, RoutedEventArgs e)
@@ -47,11 +104,23 @@ namespace JuniorSlataTestTask
         {
             if (!Application.Current.MainWindow.IsActive)
                 Application.Current.Shutdown();
+            else OnWindowClosed(EventArgs.Empty);
         }
 
-        private void Window_Closed(object sender, EventArgs e)
+        private void tbPassCheck_LostFocus(object sender, RoutedEventArgs e)
         {
-            OnWindowClosed(e);
+        }
+
+        private void KeyboardFocusEvent(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                ((TextBox)sender).Background = Brushes.Transparent;
+            }
+            if (sender is PasswordBox)
+            {
+                ((PasswordBox)sender).Background = Brushes.Transparent;
+            }
         }
     }
 }
