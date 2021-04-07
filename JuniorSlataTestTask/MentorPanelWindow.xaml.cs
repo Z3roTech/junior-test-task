0b10d6d0b8e75c1e630edb04a1a3c075574479a9
ZeroTech <shelikov98@yandex.ru>
2021-04-07 13:36:59 +0800

    Create base windows for users

---
+++ JuniorSlataTestTask/MentorPanelWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JuniorSlataTestTask
+{
+    /// <summary>
+    /// Interaction logic for MentorPanelWindow.xaml
+    /// </summary>
+    public partial class MentorPanelWindow : Window
+    {
+        public MentorPanelWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
